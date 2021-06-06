22278bb5e0e093742ada6a1583cb13ae52540ada
mosfeqanik <mosfeqanik01@gmail.com>
2021-06-06 14:43:14 +0600

    CircleAvatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(myapp());
+}
+class myapp extends StatelessWidget {
+  const myapp({Key? key}) : super(key: key);
 
-    ),
-  ));
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage("Assets/Images/mosfeq_anik.jpg"),
+              ),
+              Text("Md Mosfeq Anik"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
